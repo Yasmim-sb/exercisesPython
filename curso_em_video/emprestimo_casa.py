70c608a18ad5e1a4ead0ef7610cbc664f21d35d7
Yasmim <yasmimsabrinacp@gmail.com>
2024-05-08 14:45:19 -0300

    atualizando exercicio

--- curso_em_video/emprestimo_casa.py
+++ curso_em_video/emprestimo_casa.py
@@ -1,10 +1,13 @@
-valor_casa = float(input('digite o valor da casa: '))
-salario_comprador = float(input('digite seu salario mensal: '))
-parcelas_em_anos = int(input('em quantos anos você pretende pagar? '))
-parcelas = parcelas_em_anos/12
+valor_casa = float(input('digite o valor da casa: R$ '))
+salario_comprador = float(input('digite seu salario mensal: R$ '))
+anos = int(input('em quantos anos você pretende pagar? '))
+parcelas = valor_casa / (anos*12)
+minimo = salario_comprador * 30 / 100
 
-valor_mensal_da_parcela = valor_casa /  parcelas
-if valor_mensal_da_parcela > (salario_comprador/0.30):
-  print('seu emprestimo foi negado!')
+print(f'Para pagar uma casa no valor de R${valor_casa:.2f} em {anos} anos')
+print(f'A prestação seria de R${parcelas:.2f}')
+
+if parcelas > (minimo):
+  print('seu empréstimo foi negado!')
 else:
-  print(f'seu emprestimo foi aprovado, as parcelas ficaram de {parcelas:.2f}')
+  print(f'seu empréstimo foi aprovado!')
