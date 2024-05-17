476a9730767af5c7b9bf3058b308064614f76851
Yasmim <yasmimsabrinacp@gmail.com>
2024-05-17 16:32:21 -0300

    atualizando exercicio de manipulação de arquivos

--- florescendo_talentos/manipulacao_de_arquivos/vacinacao.py
+++ florescendo_talentos/manipulacao_de_arquivos/vacinacao.py
@@ -1,5 +1,28 @@
-import csv
-with open('registro_vacinacao.csv') as file:
-    ler_csv = csv.reader(file)
-    for row in ler_csv:
-        print(row)
+def adicionar_arquivo():
+    with open("registro_vacinacao.csv", "a") as arquivo:
+        nome = input("Qual seu nome? ")
+        idade = input("Quantos anos você tem? ")
+        vacinacao = input("Você foi vacinado? ")
+        arquivo.write(nome + ", " + idade + " anos,"+ "Foi vacinado(a)? " + vacinacao+"\n")
+
+        print("você foi registrado com sucesso!")
+def mostrar_arquivo():
+    with open("registro_vacinacao.csv", "r") as arquivo:
+       for linha in arquivo:
+           print(linha.strip())
+
+while True:
+    print("-------Menu-------\n")
+    print("1 - registrar arquivos\n")
+    print("2 - mostrar arquivos\n")
+    print("3 - encerrar execução\n")
+
+    menu = int(input("Qual opção acima você quer escolher? "))
+
+    if menu == 1:
+        adicionar_arquivo()
+    elif menu == 2:
+        mostrar_arquivo()
+    elif menu == 3:
+        print("execução encerrada!")
+        break
